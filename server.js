31c4720297b32512d5eb574142dc0a9b11eec346
Nikola Lukic <nlukic97@gmail.com>
2020-11-14 01:47:41 +0100

    zoom clone trial

---
+++ server.js
@@ -0,0 +1,37 @@
+let express = require('express')
+let app = express()
+let port = 3030;
+let server = require('http').createServer(app) //or createServer  ???
+const io = require('socket.io')(server)
+const {v4: uuidv4} = require('uuid')
+const {ExpressPeerServer} = require('peer')
+const peerServer = ExpressPeerServer(server,{
+    debug: true
+})
+app.set('view engine', 'ejs') //what is the ejs module for ? ejs library?
+app.use(express.static('./views'))
+
+
+app.use('/peerjs',peerServer) //peer 
+app.get('/',(req,res)=>{
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId: req.params.room})
+})
+
+io.on('connection',socket=>{
+    console.log(`User has connected.`);
+    socket.on('join-room',(roomId, userId)=>{
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected',userId)
+    })
+    
+    socket.on('disconnect',(socket)=>{
+        console.log(`${socket.id} left the room`);
+    })
+})
+server.listen((process.env.PORT || port),()=>{
+    console.log(`Server started at http://localhost:${port}`);
+})
---
+++ views/script.js
@@ -0,0 +1,73 @@
+const socket = io('/')
+
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true;
+
+const connectToNewUser = (userId, stream) =>{
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', uservideoStream => {
+        console.log('got stream of other person')
+        addVideoStream(video, uservideoStream)
+    })
+}
+
+const addVideoStream = (video, stream) =>{
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata',()=>{
+        video.play()
+    })
+    videoGrid.append(video)
+}
+
+//connecting to peer from client
+var peer = new Peer(undefined,{
+    path:'/peerjs',
+    host:'/',
+    port:'433'
+})
+
+let myVideoStream; //the videoStream is now available to all
+
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo,stream)
+    
+    peer.on('call',call=>{ //---> ovde se gubi nesto
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream',userVideoStream=>{
+            console.log(`User video stream received: ${userVideoStream}. Adding to our box`);
+            addVideoStream(video,userVideoStream)
+        })
+    })
+    
+    socket.on('user-connected',(userId)=>{
+        // console.log(`Another user has joined. Their id: ${userId}. Contacting them...`);
+        connectToNewUser(userId, stream)
+    })
+})
+
+peer.on('open', id=>{
+    // console.log(`Your id: ${id} and room id:${ROOM_ID}. Emiting 'join-room'`);
+    socket.emit('join-room', ROOM_ID, id)
+})  
+
+
+//------------ I have no comment the previous peer.on(call) and uncomment this to work for 1 user
+// peer.on('call',call=>{ //---> ovde se gubi nesto
+//     console.log(`Someone is calling. Call info: ${call}...answering...`);
+//     console.log(call);
+//     call.answer(myVideoStream)
+//     const video = document.createElement('video')
+//     call.on('stream',uservideoStream=>{
+//         console.log(`User video stream received: ${uservideoStream}. Adding to our box`);
+//         addVideoStream(video,uservideoStream)
+//     }) //---- somewhere here we may need to call the guy who first send out the signal
+// })
+//------------
+
